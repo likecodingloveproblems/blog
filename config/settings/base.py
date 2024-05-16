8de8941c817b8da2a9a67381be2f3793d47d0551
mam <mamehdizadeh.official@gmail.com>
2024-05-16 14:28:58 +0330

    Add content and like models

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,7 +91,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "blog.users",
-    # Your stuff: custom apps go here
+    "content_management",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- content_management/models.py
+++ content_management/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from blog.users.models import User
+
+
+class Content(models.Model):
+    title = models.CharField(verbose_name=_('title'), max_length=50)
+    text = models.TextField(verbose_name=_('text'))
+
+
+class Like(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField(verbose_name=_('value'),
+        validators=(MinValueValidator(limit_value=0), MaxValueValidator(limit_value=5),)
+    )
